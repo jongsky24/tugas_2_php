3bb2f94cc31dab4b8043ab55ab0c72485f4fc522
Muhammad Ridwan R <jongsky_1821@yahoo.com>
2019-12-12 22:50:35 +0700

    Tugas 2.1 PHP
    Tugas Niomic

---
+++ tugas2_1.php
@@ -0,0 +1,12 @@
+<?php
+echo "36 Hari = 12 Pekerja <br>";
+echo "X hari = 24 Pekerja <br>";
+echo "X = ...? <br>";
+echo "Jawaban : ";
+$x = 36;
+$y = 12;
+$w = 24;
+
+$u = $w / $y;
+$r = $x / $u;
+echo $r;
